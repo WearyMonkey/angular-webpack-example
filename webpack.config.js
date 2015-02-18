fabae56143274cbd6b4011e7a0c5d6396e99895e
Ronn Ross <ronn.ross@gmail.com>
2015-02-18 12:28:22 -0500

    exporting css to file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
   entry: './app/app.js',
   output: {
     path: './build',
     publicPath: 'www.test.com/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   externals: {
     "angular": "angular"
@@ -11,8 +14,11 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      { test: /\.scss$/, loader: 'style!css!sass'},
-      {test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['image?bypassOnDebug&optimizationLevel=7&interlaced=false']}
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')},
+      { test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['image?bypassOnDebug&optimizationLevel=7&interlaced=false']}
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].css")
+  ]
 };
